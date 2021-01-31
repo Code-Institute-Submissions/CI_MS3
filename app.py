a6d885cc69cd9a2653ace67e3e65b8433fb95d66
ssruoh <ssruoh@gmail.com>
2021-01-31 20:08:11 +0000

    Start setting up sign up functionality, signup.html

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -23,6 +24,11 @@ mongo = PyMongo(app)
 def all_recipes():
     recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
