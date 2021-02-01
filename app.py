bf90be5679987591c7ee73464fb9dece9d0d1cab
ssruoh <ssruoh@gmail.com>
2021-02-01 23:55:51 +0000

    Set up placeholder profile.html page

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ def signup():
 
         session["user"] = request.form.get("username").lower()
         flash("Signup Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("signup.html")
 
 
@@ -58,14 +59,24 @@ def login():
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 flash("Incorrect username, password or both!")
                 return redirect(url_for("login"))
+
         else:
             flash("Incorrect username, password or both!")
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
