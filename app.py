1cc2427ea6df93c9b0ec61a0aebd1515e36ffd5c
ssruoh <ssruoh@gmail.com>
2021-02-10 19:25:19 +0000

    Fix edit_recipe issue

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def new_recipe():
 
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
-    recipe = mongo.db.recipes.find_one("{_id: ObjectId(recipe_id)}")
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     categories = mongo.db.categories.find()
     return render_template(
         "edit_recipe.html", recipe=recipe, categories=categories)
