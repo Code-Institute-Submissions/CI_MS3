5fc664f039fd905d0c78cbfa67e1c9b9b26e463c
ssruoh <ssruoh@gmail.com>
2021-02-16 22:39:23 +0000

    Create 404 & 500 error functions and pages

--- app.py
+++ app.py
@@ -166,6 +166,18 @@ def delete_recipe(recipe_id):
     return redirect(url_for("all_recipes"))
 
 
+# Error pages, 404 & 500
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
